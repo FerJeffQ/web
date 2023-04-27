acb269b11285e0033397ed0abbfba317fe8631c4
Fernando Quisaguano <ferjeff92@gmail.com>
2023-04-27 16:12:59 -0500

    proyecto web 0.1

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/')
+def helloworld():
+    return 'Bienvenido'
+
+@app.route('/predict')
+def predict():
+    return 'Predicciones'
+
+if __name__ == "__main__":
+    app.run()
